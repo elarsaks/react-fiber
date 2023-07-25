fbaf6413efb19f304f799daa3536da7c364078b8
Elar Saks <elarsaks@gmail.com>
2023-07-25 22:34:25 +0300

    Re create react app.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-import { createRoot } from "react-dom/client";
-import "./styles.css";
-import { App } from "./App";
-import { Leva } from "leva";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-function Overlay() {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        pointerEvents: "none",
-        width: "100%",
-        height: "100%",
-      }}
-    ></div>
-  );
-}
-
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-    <Overlay />
-
-    <Leva collapsed />
-  </>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
